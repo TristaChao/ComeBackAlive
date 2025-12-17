22dd322be4fd49c9093f333f75a8801444209398
trista_chao <s282532@icloud.com>
2025-12-17 21:59:55 +0800

    移除測試資料

--- mouse-nest-demo/GameState.gd
+++ mouse-nest-demo/GameState.gd
@@ -10,18 +10,3 @@ var state := State.FREE_MOVE
 
 var pouch := []
 const POUCH_CAPACITY := 5
-
-func _ready():
-	# --- Temporary for testing Pouch UI ---
-	var test_beef_1 := ItemData.new()
-	test_beef_1.id = "beef_1"
-	test_beef_1.cook_state = ItemData.CookState.RAW
-	test_beef_1.texture = load("res://images/assets/food/16/tile_180.png")
-	pouch.append(test_beef_1)
-
-	var test_beef_2 := ItemData.new()
-	test_beef_2.id = "beef_2"
-	test_beef_2.cook_state = ItemData.CookState.RAW
-	test_beef_2.texture = load("res://images/assets/food/16/tile_180.png")
-	pouch.append(test_beef_2)
-	# --- End Temporary ---
