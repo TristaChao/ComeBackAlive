9ddc2a8c6942c9f5a67854a57db76e29fe9651ae
trista_chao <s282532@icloud.com>
2025-12-17 09:19:21 +0800

    可以跟鼠媽說話了

--- mouse-nest-demo/scripts/Player.gd
+++ mouse-nest-demo/scripts/Player.gd
@@ -19,14 +19,14 @@ func _physics_process(delta):
 	
 func _input(event):
 	if event.is_action_pressed("interact"):
-		print("按下 E")
+		print("嘗試開啟對話")
 		if GameState.state != GameState.State.FREE_MOVE:
 			return
 
-			for area in $InteractionArea.get_overlapping_areas():
-				if area.has_method("can_interact") and area.can_interact():
-					open_mouse_mom_dialog()
-					return
+		for area in $InteractionArea.get_overlapping_areas():
+			if area.has_method("can_interact") and area.can_interact():
+				open_mouse_mom_dialog()
+				return
 
 func open_mouse_mom_dialog():
 	var dialog = get_tree().current_scene.get_node("UI/DialogueBox")
