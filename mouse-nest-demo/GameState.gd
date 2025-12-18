54c98010d92894e5c9506ef42ebc8784fb704955
trista_chao <s282532@icloud.com>
2025-12-18 18:51:51 +0800

    「煮鍋(Stove)」和「序列料理」測試
    流程A：測試烤牛排
       1. 去倉庫拿出「生牛肉」。 OK
       2. 將「生牛肉」拿到烤台上，確認現在可以正常燒烤。OK
    
    流程B：測試蘑菇醬汁
       1. 從煮鍋取得「水」，並放入鍋中。OK
       2. 從櫃台取得「洋蔥」，並加入鍋中，煮成「洋蔥湯」。OK
       3. 去倉庫拿出「蘑菇」。OK
       4. 將「蘑菇」加入鍋中，煮成「蘑菇醬汁」。OK
       5. 從鍋中取出「蘑菇醬汁」。OK

--- mouse-nest-demo/GameState.gd
+++ mouse-nest-demo/GameState.gd
@@ -19,23 +19,17 @@ const WAREHOUSE_CAPACITY := 20
 
 func _ready():
 	# --- 臨時測試資料 ---
-	# 請注意：在完成功能並測試後，記得移除或註解掉這段程式碼！
+	# 我們在倉庫裡放入一些測試用的食材
 	
-	# 如果頰囊是空的，加入測試物品
-	if pouch.is_empty():
-		for i in range(2): # 加入 2 個物品到頰囊
-			var item = ItemData.new()
-			item.id = "test_pouch_" + str(i)
-			item.cook_state = ItemData.CookState.RAW
-			item.texture = preload("res://images/assets/food/16/tile_180.png")
-			pouch.append(item)
-
-	# 如果倉庫是空的，加入測試物品
 	if warehouse.is_empty():
-		for i in range(5): # 加入 5 個物品到倉庫
-			var item = ItemData.new()
-			item.id = "test_warehouse_" + str(i)
-			item.cook_state = ItemData.CookState.COOKED # 不同的狀態方便區分
-			item.texture = preload("res://images/assets/food/16/tile_180.png")
-			warehouse.append(item)
+		# Add a raw beef to test grilling
+		var beef_to_test = ItemDatabase.create_item("beef")
+		if beef_to_test:
+			warehouse.append(beef_to_test)
+			
+		# Add a mushroom to test sauce recipe
+		var mushroom_to_test = ItemDatabase.create_item("mushroom")
+		if mushroom_to_test:
+			warehouse.append(mushroom_to_test)
+
 	# --- 臨時測試資料結束 ---
